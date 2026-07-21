3082e4d1d1b19074fd2c2e087423d8ad8166459e
Trisha Sai Durga <trishavegi9@gmail.com>
2026-07-21 20:56:39 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-gigsphere.onrender.com/api",
 });
 
 // Automatically send JWT token with every request
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -14,7 +13,6 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
 
 export default api;
