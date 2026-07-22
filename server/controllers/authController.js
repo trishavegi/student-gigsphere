6ce35aa0c8f43198f6f34a5c3d7333c01b34c03e
Trisha Sai Durga <trishavegi9@gmail.com>
2026-07-22 21:37:26 +0530

    Fix admin profile update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -190,8 +190,7 @@ const changePassword = async (req, res) => {
 };
 const updateProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id);
-
+const user = await User.findById(req.user);
     if (!user) {
       return res.status(404).json({
         message: "User not found",
